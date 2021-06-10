ec65e6df0825adf6634005bfce1f9e2ae703b83a
scrawnycoleman <thomashutchinson450@gmail.com>
2021-06-10 17:56:25 +0100

    first commit

---
+++ src/handlers/createAuction.js
@@ -0,0 +1,40 @@
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+import createError from "http-errors";
+import commonMiddleware from "../lib/commonMiddleware";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function createAuction(event, context) {
+  const { title } = event.body;
+  const now = new Date();
+
+  const auction = {
+    id: uuid(),
+    title,
+    status: "OPEN",
+    createdAt: now.toISOString(),
+    highestBid: {
+      amount: 0,
+    },
+  };
+
+  try {
+    await dynamodb
+      .put({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Item: auction,
+      })
+      .promise();
+  } catch (err) {
+    console.error(err);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = commonMiddleware(createAuction);
