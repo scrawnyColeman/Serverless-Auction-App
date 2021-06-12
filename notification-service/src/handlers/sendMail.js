c872a15ebb363a3d66ed7a75d385c92bf8c134c4
scrawnycoleman <thomashutchinson450@gmail.com>
2021-06-12 14:30:33 +0100

    message queue

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -3,27 +3,35 @@ import AWS from "aws-sdk";
 const ses = new AWS.SES({ region: "eu-west-1" });
 
 async function sendMail(event, context) {
+  const record = event.Records[0];
+  console.log("record processing", record);
+
+  const email = JSON.parse(record.body);
+  const { subject, body, recipient } = email;
+
   const params = {
     Source: "thomashutchinson450@gmail.com",
     Destination: {
-      ToAddresses: ["thomashutchinson450@gmail.com"],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: "Hello from Udemy",
+          Data: body,
         },
       },
       Subject: {
-        Data: "Test Mail",
+        Data: subject,
       },
     },
   };
+
   try {
     const result = await ses.sendEmail(params).promise();
     console.log(result);
-  } catch (err) {
-    console.error(err);
+    return result;
+  } catch (error) {
+    console.error(error);
   }
 }
 
