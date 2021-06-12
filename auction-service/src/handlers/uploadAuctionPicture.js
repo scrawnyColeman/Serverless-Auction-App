e70bc84499f97441ba968758570345d24992327b
scrawnycoleman <thomashutchinson450@gmail.com>
2021-06-12 23:18:05 +0100

    Adding vslidated image to S3 and url to dynamodb

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -1,8 +1,43 @@
+import middy from "@middy/core";
+import httpErrorHandler from "@middy/http-error-handler";
+import validator from "@middy/validator";
+import createError from "http-errors";
+import { setAuctionPictureUrl } from "../lib/setAuctionPictureUrl";
+import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
+import { getAuctionById } from "./getAuction";
+import uploadAuctionPictureSchema from "../lib/schemas/uploadAuctionPictureSchema";
+
 export async function uploadAuctionPicture(event) {
+  const { id } = event.pathParameters;
+  const { email } = event.requestContext.authorizer;
+  const auction = await getAuctionById(id);
+
+  if (auction.sellerEmail !== email)
+    throw new createError.Forbidden("You are not the seller");
+
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
+  const buffer = Buffer.from(base64, "base64");
+
+  if (buffer.toString("base64") !== base64) {
+    throw new createError.BadRequest("Invalid image supplied.");
+  }
+
+  let updatedAuction;
+
+  try {
+    const pictureUrl = await uploadPictureToS3(auction.id + ".jpg", buffer);
+    updatedAuction = await setAuctionPictureUrl(id, pictureUrl);
+  } catch (err) {
+    console.error(err);
+    throw new createError.InternalServerError(err);
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: JSON.stringify(updatedAuction),
   };
 }
 
-export const handler = uploadAuctionPicture;
+export const handler = middy(uploadAuctionPicture)
+  .use(httpErrorHandler())
+  .use(validator({ inputSchema: uploadAuctionPictureSchema }));
